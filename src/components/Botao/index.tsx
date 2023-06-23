09b2a15da012e4f4a2d9dca90f189d06cd31eea0
Nickolas ramos <nickrferreti6@gmail.com>
2023-06-23 18:13:00 -0300

    Commit do zero

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Botao/style.scss'
+
+class Botao extends React.Component {
+  render() {
+    return (
+      <button
+      className='botao'>
+        Botão
+      </button>
+    )
+  }
+}
+
+export default Botao;
