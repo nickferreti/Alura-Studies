318f569e51f9b8170336b5d1b2f80fcdb112d902
Nickolas ramos <nickrferreti6@gmail.com>
2023-06-28 20:08:45 -0300

    Corrigindo erro NaN

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
 import Relogio from "./Relogio";
@@ -11,9 +11,13 @@ interface Props {
 
 export default function Cronometro({selecionado}: Props) {
     const [tempo, setTempo] = useState<number>();
-    if(selecionado?.tempo) {
-        setTempo(tempoSegundos(selecionado.tempo));
-    }
+
+    useEffect(()=> {
+        if(selecionado?.tempo) {
+            setTempo(tempoSegundos(selecionado.tempo))
+        }
+    }, [selecionado])
+
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
